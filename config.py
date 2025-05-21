299bd0119a311f4c332809af36063e8645f52bd2
UniqeXD <shubhogarai730@gmail.com>
2025-05-21 08:14:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/iambuddy01/GaanaV2",
+    "https://github.com/UniqeXD/Uniqe_music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
